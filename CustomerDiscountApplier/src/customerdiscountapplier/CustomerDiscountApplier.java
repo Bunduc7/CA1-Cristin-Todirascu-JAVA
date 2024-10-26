29c96be639379b36538b8e939a8963a20f502ae1
Bunduc7 <cristotodirascu7@gmail.com>
2024-10-26 18:14:54 +0100

    1st commit

---
+++ CustomerDiscountApplier/src/customerdiscountapplier/CustomerDiscountApplier.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package customerdiscountapplier; 
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
