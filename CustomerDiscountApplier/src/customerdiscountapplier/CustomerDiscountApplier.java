f97af7b7fe9d41e982dff5f4ee77924a571afa10
Bunduc7 <cristotodirascu7@gmail.com>
2024-10-26 18:27:16 +0100

    Initialize project structure and define main class

--- CustomerDiscountApplier/src/customerdiscountapplier/CustomerDiscountApplier.java
+++ CustomerDiscountApplier/src/customerdiscountapplier/CustomerDiscountApplier.java
@@ -2,10 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package customerdiscountapplier; 
+package customerdiscountapplier;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+
+public class CustomerDiscountApplier {
+
+    public static void main(String[] args) {
+        String inputFile = "customers.txt"; // Input file with customer data
+        String outputFile = "customerdiscount.txt"; // Output file for discounts
